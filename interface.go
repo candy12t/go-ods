69c7bc668c96e4ca7e008f276f6d534ff7ee730f
Takumi Kanada <candy12t@gmail.com>
2023-11-07 17:16:14 +0900

    Add: interface

---
+++ interface.go
@@ -0,0 +1,46 @@
+package ods
+
+type V any
+
+// Queue is FIFO (first-in-first-out) queue
+type Queue interface {
+	Enqueue(V)
+	Dequeue() V
+}
+
+// Stack is LIFO (last-in-first-out) queue
+type Stack interface {
+	Push(V)
+	Pop() V
+}
+
+type Deque interface {
+	AddFirst(V)
+	AddLast(V)
+	RemoveFirst() V
+	RemoveLast() V
+}
+
+type List interface {
+	Size() int
+	Get(int) V
+	Set(int, V) V
+	Add(int, V)
+	Remove(int) V
+}
+
+// USet is unordered Set
+type USet interface {
+	Size() int
+	Add(int, V)
+	Remove(int) V
+	Find(V) V
+}
+
+// SSet is sorted Set
+type SSet interface {
+	Size() int
+	Add(int, V)
+	Remove(int) V
+	Find(V) V
+}
